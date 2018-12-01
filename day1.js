50c209840acb936bcac669e1350f7fe744c7a000
Matt MacAulay <mac@twg.io>
2018-12-01 15:02:09 -0500

    Day 1 part 2

--- day1.js
+++ day1.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 const lines = fs.readFileSync('day1.txt', { encoding: 'utf8' }).split('\n')
 
-function run () {
+function part1 () {
   let freq = 0
   lines.forEach(line => {
     const op = line.slice(0, 1)
@@ -12,4 +12,21 @@ function run () {
   console.log(freq)
 }
 
-run()
+function part2 () {
+  let map = {}
+  let freq = 0
+  while (true) {
+    for (let i = 0; i < lines.length; i++) {
+      const line = lines[i]
+      const op = line.slice(0, 1)
+      const n = parseInt(line.slice(1), 10)
+      if (op === '+') freq += n
+      else freq -= n
+      if (map[freq.toString()]) return console.log(freq)
+      map[freq.toString()] = true
+    }
+  }
+}
+
+// part1()
+part2()
